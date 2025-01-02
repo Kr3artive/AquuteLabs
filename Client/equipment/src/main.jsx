8f5d4a29eee10e9234153632e7e32deb7b9bbc22
Abiye <abiyerowland@gmail.com>
2025-01-02 05:25:43 +0200

    I just confirmed that i'm good at this

--- Client/equipment/src/main.jsx
+++ Client/equipment/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import EquipmentProvider from "./contexts/EquipmentContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
